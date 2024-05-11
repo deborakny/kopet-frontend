dc4adfd4317fb57cc87b73bbbb54f936f64f99ed
Debora Kuniy <debora.n_@hotmail.com>
2024-05-11 18:14:18 -0300

    banner dinamico

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit{
+  @Input() logo: boolean = false;
+  classeBanner: string = '';
+  ngOnInit(): void {
+    this.classeBanner = this.logo ? 'banner' : 'banner-sem-logo';
 
+  }
 }
