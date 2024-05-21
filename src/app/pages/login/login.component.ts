a0354478aed9307ec91c8b10258786b059f60f52
Debora Kuniy <debora.n_@hotmail.com>
2024-05-21 01:54:58 -0300

    implementacao do login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private autenticacaoService: AutenticacaoService,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, [Validators.required]],
+    });
+  }
+
+  logIn() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const senha = this.loginForm.value.senha;
+      this.autenticacaoService.autenticar(email, senha).subscribe({
+        next: (res) => {
+          this.snackbar.open('Usuário logado', '', {
+            horizontalPosition: 'center',
+            verticalPosition: 'bottom',
+            duration: 3000,
+            panelClass: ['custom-snackbar'],
+          });
+          this.router.navigate(['/']);
+        },
+        error: (e) => {
+          console.log(e);
+
+          this.snackbar.open('Erro ao logar', '', {
+            horizontalPosition: 'center',
+            verticalPosition: 'bottom',
+            duration: 3000,
+            panelClass: ['custom-snackbar'],
+          });
+        },
+      });
+    } else {
+      this.snackbar.open('Usuário inválido', '', {
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        duration: 3000,
+        panelClass: ['custom-snackbar'],
+      });
+    }
+  }
 }
