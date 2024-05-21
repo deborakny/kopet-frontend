013ae872d0fb0ace39ed08746e6f653a59da7c3b
Debora Kuniy <debora.n_@hotmail.com>
2024-05-21 02:23:06 -0300

    getpets no service

---
+++ src/app/core/services/pet.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Pet } from '../types/pet';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+
+  private readonly apiUrl: string = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPetsByCliente(idCliente: number): Observable<Pet[]> {
+    return this.http.get<Pet[]>(`${this.apiUrl}/pets/cliente/${idCliente}`);
+  }
+}
