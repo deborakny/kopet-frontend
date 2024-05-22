7c458406eedf31248c8f995e39e81ffd01501452
Debora Kuniy <debora.n_@hotmail.com>
2024-05-21 22:34:49 -0300

    botoes dinamicos no header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContaService } from 'src/app/core/services/conta.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  logado: boolean = false;
+
+  constructor(
+    private contaService: ContaService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.contaService.logado()) {
+      this.logado = true;
+    } else {
+      this.logado = false
+    }
+  }
 }
