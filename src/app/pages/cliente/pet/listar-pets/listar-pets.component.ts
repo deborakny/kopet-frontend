72f2a972fa75459db0fd7ae4fb9ea00f230c70c6
Debora Kuniy <debora.n_@hotmail.com>
2024-06-03 16:11:19 -0300

    interface do formulario de cadastrar pet

--- src/app/pages/cliente/pet/listar-pets/listar-pets.component.ts
+++ src/app/pages/cliente/pet/listar-pets/listar-pets.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pets',
   templateUrl: './listar-pets.component.html',
   styleUrls: ['./listar-pets.component.scss']
 })
-export class ListarPetsComponent {
+export class ListarPetsComponent implements OnInit{
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
