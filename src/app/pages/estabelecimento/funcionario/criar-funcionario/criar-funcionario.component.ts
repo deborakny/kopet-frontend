246c2405b40628d9f501cb48464a1d0c9ad6428c
Debora Kuniy <debora.n_@hotmail.com>
2024-06-05 15:21:12 -0300

    preenchimento dos servicos do estabelecimento no formulario de cadastro de funcionario

--- src/app/pages/estabelecimento/funcionario/criar-funcionario/criar-funcionario.component.ts
+++ src/app/pages/estabelecimento/funcionario/criar-funcionario/criar-funcionario.component.ts
@@ -1,3 +1,5 @@
+import { ServicoService } from 'src/app/core/services/servico.service';
+import { Servico } from './../../../../core/types/servico';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ContaService } from 'src/app/core/services/conta.service';
@@ -10,15 +12,26 @@ import { ContaService } from 'src/app/core/services/conta.service';
 export class CriarFuncionarioComponent implements OnInit{
 
   usuarioLogado?: boolean;
-  servicos = new FormControl('');
-  servicosList: string[] = ['Banho e Tosa', 'Veterinário', 'Hidratação', 'Tosa Higiênica'];
+  servicosList: Servico[] = [];
 
   constructor(
-    private contaService: ContaService
+    private contaService: ContaService,
+    private servicoService: ServicoService
   ) { }
 
   ngOnInit(): void {
     this.usuarioLogado = this.contaService.logado();
+
+    // const estabelecimentoId = this.contaService.getId();
+
+    const estabelecimentoId = 31;
+    this.getServicos(estabelecimentoId);
+  }
+
+  getServicos(id: number) {
+    this.servicoService.getServicosByEstabelecimento(id).subscribe(res => {
+      this.servicosList = res
+    })
   }
 
 }
