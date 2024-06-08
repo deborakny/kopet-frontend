e22c0c76120c17373b0713ba54d0df956c481d58
Debora Kuniy <debora.n_@hotmail.com>
2024-06-08 10:43:50 -0300

    finalizacao do component perfil-cliente e atualizacao do header

--- src/app/pages/cliente/perfil-cliente/perfil-cliente.component.ts
+++ src/app/pages/cliente/perfil-cliente/perfil-cliente.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/core/services/cliente.service';
+import { ContaService } from 'src/app/core/services/conta.service';
+import { Cliente } from 'src/app/core/types/cliente';
 
 @Component({
   selector: 'app-perfil-cliente',
   templateUrl: './perfil-cliente.component.html',
   styleUrls: ['./perfil-cliente.component.scss']
 })
-export class PerfilClienteComponent {
+export class PerfilClienteComponent implements OnInit{
+
+  cliente!: Cliente;
+
+  constructor(
+    private contaService: ContaService,
+    private clienteService: ClienteService
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.contaService.getId();
+    //const id = 26;
+    this.getCliente(id!);
+  }
+
+  getCliente(id: number) {
+    this.clienteService.getCliente(id).subscribe(
+      res => {
+        this.cliente = res;
+      }
+    );
+  }
 
 }
