56177cd05ce1f8f22436b10e5e558519df215302
Debora Kuniy <debora.n_@hotmail.com>
2024-06-17 13:23:40 -0300

    exclusao de servico

--- src/app/shared/card-servico/card-servico.component.ts
+++ src/app/shared/card-servico/card-servico.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ContaService } from 'src/app/core/services/conta.service';
+import { ServicoService } from 'src/app/core/services/servico.service';
+import { ConfirmaExclusaoDialogComponent } from '../confirma-exclusao-dialog/confirma-exclusao-dialog.component';
 
 @Component({
   selector: 'app-card-servico',
@@ -15,7 +19,10 @@ export class CardServicoComponent implements OnInit{
   estabelecimentoId?: number;
 
   constructor(
-    private contaService: ContaService
+    private contaService: ContaService,
+    public dialog: MatDialog,
+    private servicoService: ServicoService,
+    private snackbar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -26,6 +33,30 @@ export class CardServicoComponent implements OnInit{
     // } else {
     //   this.valorString = "Não informado"
     // }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmaExclusaoDialogComponent, {
+      width: '250px',
+    });
+    dialogRef.componentInstance.confirmaExclusao.subscribe(
+      res => {
+        if (res) {
+          this.servicoService.excluir(this.servico.id).subscribe({
+            next: (value) => {
+              console.log('Excluído');
+              this.snackbar.open('Serviço excluído', '', {
+                horizontalPosition: "center", verticalPosition: "bottom", duration: 3000
+              });
+              window.location.reload();
+            },
+            error: (err) => {
+              console.log('erro', err)
+            }
+          })
+        }
+      }
+    );
   }
 
 
